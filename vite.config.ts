14944ea02797eaa973bce27a286d8869e54f7b71
Bram Lahousse <70818886+LahousseBram@users.noreply.github.com>
2025-05-30 17:24:39 +0200

    netlify

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,26 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-import fs from 'fs'
+import fs from "fs";
 
-
-export default defineConfig({
-  build: {
-    outDir: 'dist',
-    // after build hook to create _redirects file
-    rollupOptions: {
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [
+    react(),
+    mode === 'development' &&
+    componentTagger(),
+  ].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  base: '/',
+  rollupOptions: {
       plugins: [{
         name: 'create-redirects',
         writeBundle() {
@@ -18,5 +30,4 @@ export default defineConfig({
         }
       }]
     }
-  }
-})
+}));
